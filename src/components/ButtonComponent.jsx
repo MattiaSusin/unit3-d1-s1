2faa13c5e82fbf7f86797368d866365825507795
MattiaSusin <m.susin28@gmail.com>
2024-06-24 17:24:10 +0200

    s

---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Button = props => {
+
+return (
+
+    <button>CLICK HERE</button>
+
+);
+
+};
+
+export default Button;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header(props) {
+      return (
+        <div>
+        <button>PORCO DIO</button>
+        </div>
+
+        )
+}
+    
+
+export default Header;
